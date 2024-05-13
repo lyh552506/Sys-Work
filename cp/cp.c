60a83976b8171c73e0a71053f7440489e06f01a5
lyh552506 <1661650252@qq.com>
2024-05-13 16:05:33 +0800

    update

--- cp/cp.c
+++ cp/cp.c
@@ -1,26 +1,48 @@
-#include <cstddef>
-#include <cstdlib>
-#include <cstring>
 #include <dirent.h>
 #include <fcntl.h>
 #include <grp.h>
 #include <pwd.h>
 #include <stdbool.h>
+#include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <time.h>
 #include <unistd.h>
 #define MAX 100
 
 void FileToFile(char *src, char *dst) {
+  char buff[512];
   if (access(src, F_OK) == -1) {
     printf("cp: cannot stat '%s': No such file or directory", src);
     exit(0);
   }
   int fd_dst = open(dst, O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR);
+  int fd_src = open(src, O_RDONLY);
+  ssize_t bufread=0;
+  while((bufread=read(fd_src,buff,sizeof(buff)))>0){
+    write(fd_dst, buff, bufread);
+  }
+  close(fd_dst);
+  close(fd_src);
 }
 
-void DirToDir(char *src, char *dst) {}
+void DirToDir(char *src, char *dst) {
+  DIR* dir=NULL;
+  struct dirent* read;
+  if((dir=opendir(src))==NULL){
+    printf("cp: cannot stat '%s': No such file or directory", src);
+    exit(0);
+  }
+  while((read = readdir(dir)) != NULL){
+    char *name=read->d_name;
+    char file_path[1024];
+    snprintf(file_path, sizeof(file_path), src, name);
+
+  }
+}
 
 int main(int argc, char *argv[]) {
   char option;
