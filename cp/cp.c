8c895d4fbd0420fe8f9fe71c77cb8326e1372334
lyh552506 <1661650252@qq.com>
2024-05-15 11:39:13 +0800

    [CP] update different situation

--- cp/cp.c
+++ cp/cp.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <dirent.h>
 #include <fcntl.h>
 #include <grp.h>
@@ -15,22 +16,69 @@
 
 void FileToFile(char *src, char *dst) {
   char buff[512];
+  char choice;
+  bool IsCover = false, IsCat = false, Drop = false;
   if (access(src, F_OK) == -1) {
     printf("cp: cannot stat '%s': No such file or directory", src);
     exit(0);
   }
-  int fd_dst = open(dst, O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR);
-  int fd_src = open(src, O_RDONLY);
-  ssize_t bufread = 0;
-  while ((bufread = read(fd_src, buff, sizeof(buff))) > 0) {
-    write(fd_dst, buff, bufread);
+  if (access(dst, F_OK) != -1) {
+    printf("current dst has same dst %s, Should We Cover? [Y/N]\n", dst);
+    scanf("%c", &choice);
+    getchar();
+    if (choice == 'Y' || choice == 'y')
+      IsCover = true;
+    else if (choice == 'N' || choice == 'n') {
+      printf(" Should We Cat two files? [Y/N]\n");
+      scanf("%c", &choice);
+      if (choice == 'Y' || choice == 'y')
+        IsCat = true;
+      else if (choice == 'N' || choice == 'n') {
+        printf("then we will drop this file\n");
+        Drop = true;
+      } else {
+        perror("wrong input");
+      }
+    } else {
+      perror("wrong input");
+    }
   }
-  close(fd_dst);
-  close(fd_src);
+  if (!IsCover && !IsCat && !Drop) {
+    int fd_dst = open(dst, O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR);
+    int fd_src = open(src, O_RDONLY);
+    ssize_t bufread = 0;
+    while ((bufread = read(fd_src, buff, sizeof(buff))) > 0) {
+      write(fd_dst, buff, bufread);
+    }
+    close(fd_dst);
+    close(fd_src);
+  } else if (IsCover && !IsCat && !Drop) {
+    int fd_dst = open(dst, O_CREAT | O_WRONLY|O_TRUNC);
+    int fd_src = open(src, O_RDONLY);
+    ssize_t bufread = 0;
+    while ((bufread = read(fd_src, buff, sizeof(buff))) > 0) {
+      write(fd_dst, buff, bufread);
+    }
+    close(fd_dst);
+    close(fd_src);
+  } else if (!IsCover && IsCat && !Drop) {
+    int fd_dst = open(dst, O_WRONLY|O_APPEND);
+    int fd_src = open(src, O_RDONLY);
+    ssize_t bufread = 0;
+    while ((bufread = read(fd_src, buff, sizeof(buff))) > 0) {
+      write(fd_dst, buff, bufread);
+    }
+    close(fd_dst);
+    close(fd_src);
+  } else if (!IsCover && !IsCat && Drop) {
+    return;
+  } else {
+    assert(0);
+  }
 }
 
 void DirToDir(char *src, char *dst) {
-  DIR *dir = NULL;
+  DIR *dir = NULL, *subdir = NULL;
   struct dirent *read;
   if ((dir = opendir(src)) == NULL) {
     printf("cp: cannot stat '%s': No such file or directory", src);
@@ -41,6 +89,10 @@ void DirToDir(char *src, char *dst) {
     char file_src_path[1024], file_dst_path[1024];
     sprintf(file_src_path, "%s%s%s", src, "/", name);
     sprintf(file_dst_path, "%s%s%s", dst, "/", name);
+    if ((subdir = opendir(file_src_path)) != NULL) {
+      DirToDir(file_src_path, file_dst_path);
+      continue;
+    }
     FileToFile(file_src_path, file_dst_path);
   }
 }
